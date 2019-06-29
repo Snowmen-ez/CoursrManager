67051df4884ec33b0a641c46f0df69ffb68a1e43
esrge <thedh>
2019-06-29 13:12:55 +0800

    创建项目

---
+++ LEZ-ez/CourseManager/CourseManager/Models/Text.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseManager.Models
+{
+    public class Text
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Name { get; set; }
+    }
+}
